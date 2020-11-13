8b88808ec10ec7abf3c3a31937a757dec893795b
Keith Whitley <kwhitley@redhat.com>
2020-11-13 16:26:15 -0500

    testing airflow

--- dags/openshift_nightlies/tasks/install_cluster/task.py
+++ dags/openshift_nightlies/tasks/install_cluster/task.py
@@ -5,8 +5,7 @@ def get_task(dag, platform, version, config):
     return BashOperator(
         task_id=f"install_openshift_{version}_{platform}",
         depends_on_past=False,
-        bash_command='echo Im alive!!!!!!',
-        #bash_command=f"/opt/airflow/dags/repo/dags/openshift_nightlies/tasks/install_cluster/scripts/install_cluster.sh -p {platform} -v {version} -j {config}",
+        bash_command=f"chmod +x /opt/airflow/dags/repo/dags/openshift_nightlies/tasks/install_cluster/scripts/install_cluster.sh; /opt/airflow/dags/repo/dags/openshift_nightlies/tasks/install_cluster/scripts/install_cluster.sh -p {platform} -v {version} -j {config}",
         retries=3,
         dag=dag,
 )
