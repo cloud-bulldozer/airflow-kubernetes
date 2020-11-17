6c532e60147d15d56cb9b7723f02cb87d06e2173
Keith Whitley <kwhitley@redhat.com>
2020-11-17 15:23:07 -0500

    still testing airflow :)

--- dags/openshift_nightlies/tasks/install_cluster/scripts/install_cluster.sh
+++ dags/openshift_nightlies/tasks/install_cluster/scripts/install_cluster.sh
@@ -19,7 +19,7 @@ git clone https://github.com/openshift-scale/scale-ci-deploy
 git clone https://${SSHKEY_TOKEN}@github.com/redhat-performance/perf-dept.git
 export PUBLIC_KEY=/home/airflow/workspace/perf-dept/ssh_keys/id_rsa_pbench_ec2.pub
 export PRIVATE_KEY=/home/airflow/workspace/perf-dept/ssh_keys/id_rsa_pbench_ec2 
-export OPENSHIFT_INSTALL_SSH_PUB_KEY_FILE=/home/airflow/workspace/perf-dept/ssh_keys/id_rsa_pbench_ec2.pub
+export ANSIBLE_FORCE_COLOR=true
 chmod 600 ${PRIVATE_KEY}
 
 
@@ -33,4 +33,4 @@ cat inventory
 
 cat ${json_file}
 echo "ANSIBLE_DEBUG=True ansible-playbook -vvvv -i inventory OCP-$version.X/install-on-$platform.yml --extra-vars @${json_file}"
-ansible-playbook -vv -i inventory OCP-$version.X/install-on-$platform.yml --extra-vars "@${json_file}"
+ANSIBLE_DEBUG=true ansible-playbook -vv -i inventory OCP-$version.X/install-on-$platform.yml --extra-vars "@${json_file}"
