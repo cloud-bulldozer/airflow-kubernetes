d6bfde14f4ca878e2560d3075fabca8f9a8173e8
Keith Whitley <kwhitley@redhat.com>
2020-11-17 11:03:49 -0500

    still testing airflow :)

--- dags/openshift_nightlies/tasks/install_cluster/scripts/install_cluster.sh
+++ dags/openshift_nightlies/tasks/install_cluster/scripts/install_cluster.sh
@@ -28,4 +28,4 @@ echo "${ORCHESTRATION_HOST}" >> inventory
 
 cat inventory
 
-ANSIBLE_DEBUG=True ansible-playbook -vvvv -i inventory OCP-$version.X/install-on-$platform.yml --extra-vars ${json_string}
+ANSIBLE_DEBUG=True ansible-playbook -vvvv -i inventory OCP-$version.X/install-on-$platform.yml --list-tasks --extra-vars ${json_string}
