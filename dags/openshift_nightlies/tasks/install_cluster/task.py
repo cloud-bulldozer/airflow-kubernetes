da6364f8467d0339bbaa71061ebd0c07f191457c
Keith Whitley <kwhitley@redhat.com>
2020-11-17 11:28:01 -0500

    still testing airflow :)

--- dags/openshift_nightlies/tasks/install_cluster/task.py
+++ dags/openshift_nightlies/tasks/install_cluster/task.py
@@ -20,7 +20,7 @@ def get_task(dag, platform, version, config):
     }
 
     with open('/home/airflow/task.json', 'w') as json_file:
-        data = json.dump(config)
+        json.dump(config, json_file)
     
     return BashOperator(
         task_id=f"install_openshift_{version}_{platform}",
