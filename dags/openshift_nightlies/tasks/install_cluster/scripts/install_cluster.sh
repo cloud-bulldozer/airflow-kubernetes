4c4381f030d3974f0204244cc839929699f6385c
Keith Whitley <kwhitley@redhat.com>
2020-11-17 15:10:27 -0500

    still testing airflow :)

--- dags/openshift_nightlies/tasks/install_cluster/scripts/install_cluster.sh
+++ dags/openshift_nightlies/tasks/install_cluster/scripts/install_cluster.sh
@@ -17,8 +17,8 @@ mkdir /home/airflow/workspace
 cd /home/airflow/workspace
 git clone https://github.com/openshift-scale/scale-ci-deploy
 git clone https://${SSHKEY_TOKEN}@github.com/redhat-performance/perf-dept.git
-export PUBLIC_KEY=perf-dept/ssh_keys/id_rsa_pbench_ec2.pub
-export PRIVATE_KEY=perf-dept/ssh_keys/id_rsa_pbench_ec2 
+export PUBLIC_KEY=/home/airflow/workspace/perf-dept/ssh_keys/id_rsa_pbench_ec2.pub
+export PRIVATE_KEY=/home/airflow/workspace/perf-dept/ssh_keys/id_rsa_pbench_ec2 
 chmod 600 ${PRIVATE_KEY}
 
 
@@ -27,10 +27,8 @@ cd scale-ci-deploy
 echo "[orchestration]" > inventory
 echo "${ORCHESTRATION_HOST}" >> inventory
 
-export CERBERUS_ENABLE=false
 
 cat inventory
-printenv
 
 cat ${json_file}
 echo "ANSIBLE_DEBUG=True ansible-playbook -vvvv -i inventory OCP-$version.X/install-on-$platform.yml --extra-vars @${json_file}"
