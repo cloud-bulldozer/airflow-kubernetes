bea770a1f9cfaea60b26619b46cecca84f89e95b
Keith Whitley <kwhitley@redhat.com>
2020-11-17 11:26:47 -0500

    still testing airflow :)

--- dags/openshift_nightlies/tasks/install_cluster/task.py
+++ dags/openshift_nightlies/tasks/install_cluster/task.py
@@ -18,11 +18,14 @@ def get_task(dag, platform, version, config):
         "SSHKEY_TOKEN": aws['sshkey_token'],
         "ORCHESTRATION_HOST": aws['orchestration_host']
     }
+
+    with open('/home/airflow/task.json', 'w') as json_file:
+        data = json.dump(config)
     
     return BashOperator(
         task_id=f"install_openshift_{version}_{platform}",
         depends_on_past=False,
-        bash_command=f"/opt/airflow/dags/repo/dags/openshift_nightlies/tasks/install_cluster/scripts/install_cluster.sh -p {platform} -v {version} -j '{json.dumps(config)}'",
+        bash_command=f"/opt/airflow/dags/repo/dags/openshift_nightlies/tasks/install_cluster/scripts/install_cluster.sh -p {platform} -v {version} -j @/home/airflow/task.json",
         retries=3,
         dag=dag,
         executor_config=exec_config,
