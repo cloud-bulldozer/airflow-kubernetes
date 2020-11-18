350f43a2f03a1b55598ace2eb73259c1e4850db1
Keith Whitley <kwhitley@redhat.com>
2020-11-18 15:03:43 -0500

    general refactoring

--- dags/openshift_nightlies/tasks/install_cluster.py
+++ dags/openshift_nightlies/tasks/install_cluster.py
@@ -4,7 +4,7 @@ from airflow.models import Variable
 
 exec_config = {
     "KubernetesExecutor": {
-        "image": "quay.io/keithwhitley4/airflow-ansible:new-ansible-3"
+        "image": "quay.io/keithwhitley4/airflow-ansible:root-user"
     }
 }
 
