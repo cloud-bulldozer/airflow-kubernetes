7c3f6f5974de15f54154f1c04d025cd253dddb80
Keith Whitley <kwhitley@redhat.com>
2020-11-18 16:10:25 -0500

    general refactoring

--- dags/openshift_nightlies/scripts/install_cluster.sh
+++ dags/openshift_nightlies/scripts/install_cluster.sh
@@ -28,5 +28,5 @@ cd scale-ci-deploy
 echo "[orchestration]" > inventory
 echo "${ORCHESTRATION_HOST}" >> inventory
 cat inventory
-cat ${json_string}
+echo ${json_string}
 ansible-playbook -vv -i inventory OCP-$version.X/install-on-$platform.yml --extra-vars '$json_string'
