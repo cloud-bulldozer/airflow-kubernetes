a87f549414513ac0dd039c8f1a73d0e618de00dd
Keith Whitley <kwhitley@redhat.com>
2020-12-11 11:38:31 -0500

    debugging

--- dags/openshift_nightlies/tasks/benchmarks/ripsaw.py
+++ dags/openshift_nightlies/tasks/benchmarks/ripsaw.py
@@ -39,7 +39,7 @@ class Ripsaw():
         print(benchmarks)
         benchmark_operators = self._get_benchmarks(benchmarks)
         print(*benchmark_operators)
-        print(benchmark_operators[1])
+        print(benchmark_operators[1][1])
         chain(upstream, *benchmark_operators, downstream)
 
         
