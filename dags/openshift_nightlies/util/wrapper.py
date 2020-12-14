ae954dedf2abcbe7204bd447252876cf6d74bbf6
Keith Whitley <kwhitley@redhat.com>
2020-12-14 15:09:35 -0500

    adding workaround

--- dags/openshift_nightlies/util/wrapper.py
+++ dags/openshift_nightlies/util/wrapper.py
@@ -14,20 +14,25 @@ class ConditionalTask():
         self.branch_task = BranchPythonOperator(
             dag=self.dag,
             task_id=f"branch_{self.task.task_id}",
-            python_callable=self._get_task
+            python_callable=get_task,
+            op_args=[self.condition, self.task, self.dummy_task]
         )
 
         self.branch_task >> self.task 
         self.branch_task >> self.dummy_task 
 
-    def _get_task(self):
-        if condition: 
-            return self.task.task_id
-        else:
-            return self.dummy_task.task_id
+        
 
     def get_branch_task(self):
         return self.branch_task
     
     def get_leaf_tasks(self):
         return (self.task, self.dummy_task)
+
+
+
+def get_task(condition, task, dummy_task):
+    if condition: 
+        return task.task_id
+    else:
+        return dummy_task.task_id
