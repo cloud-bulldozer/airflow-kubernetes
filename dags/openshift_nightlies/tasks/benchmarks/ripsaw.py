d3cd026f79eb6357b1bc563cb5687b4a93021d00
Keith Whitley <kwhitley@redhat.com>
2020-12-21 11:41:06 -0500

    changing how benchmarks work

--- dags/openshift_nightlies/tasks/benchmarks/ripsaw.py
+++ dags/openshift_nightlies/tasks/benchmarks/ripsaw.py
@@ -37,7 +37,7 @@ class Ripsaw():
         self.vars = var_loader.build_task_vars(task="benchmarks", version=version, platform=platform, profile=profile)
         self.version_secrets = Variable.get(f"openshift_install_{version}", deserialize_json=True)
 
-    def get_benchmarks(self, benchmarks):
+    def get_benchmarks(self, benchmarks=self.vars["benchmarks"]):
         for index, benchmark in enumerate(benchmarks):
             if isinstance(benchmark, str):
                 benchmarks[index] = self._get_benchmark(benchmark)
