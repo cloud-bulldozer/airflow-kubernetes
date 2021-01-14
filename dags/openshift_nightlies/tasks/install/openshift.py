615baf17ae118bd6a389f1f743e1e634338b5551
Keith Whitley <kwhitley@redhat.com>
2021-01-14 16:05:58 -0500

    changing json file name

--- dags/openshift_nightlies/tasks/install/openshift.py
+++ dags/openshift_nightlies/tasks/install/openshift.py
@@ -64,13 +64,13 @@ class OpenshiftInstaller():
         
 
         # Dump all vars to json file for Ansible to pick up
-        with open(f"/tmp/{self.version}-{self.platform}-{self.profile}-{operation}_task.json", 'w') as json_file:
+        with open(f"/tmp/{self.version}-{self.platform}-{self.profile}-{operation}-task.json", 'w') as json_file:
             json.dump(config, json_file, sort_keys=True, indent=4)
 
         return BashOperator(
             task_id=f"{operation}",
             depends_on_past=False,
-            bash_command=f"/opt/airflow/dags/repo/dags/openshift_nightlies/scripts/install_cluster.sh -p {self.platform} -v {self.version} -j /tmp/{self.version}-{self.platform}-{self.profile}-{operation}_task.json -o {operation}",
+            bash_command=f"/opt/airflow/dags/repo/dags/openshift_nightlies/scripts/install_cluster.sh -p {self.platform} -v {self.version} -j /tmp/{self.version}-{self.platform}-{self.profile}-{operation}-task.json -o {operation}",
             retries=3,
             dag=self.dag,
             trigger_rule=trigger_rule,
