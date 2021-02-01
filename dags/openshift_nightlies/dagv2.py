3535ed71789efb2dbcc351e149ed763726e48bab
Keith Whitley <kwhitley@redhat.com>
2021-02-01 10:59:30 -0500

    testing TaskGroups

--- dags/openshift_nightlies/dagv2.py
+++ dags/openshift_nightlies/dagv2.py
@@ -49,14 +49,13 @@ class OpenshiftNightlyDAG():
         )
     
     def build(self):
-        with self.dag as dag:
-            installer = self._get_openshift_installer()
-            install_cluster = installer.get_install_task()
-            cleanup_cluster = installer.get_cleanup_task()
-            with TaskGroup("benchmarks", prefix_group_id=False) as benchmarks:
-                benchmark_tasks = self._get_ripsaw().get_benchmarks()
-                chain(*benchmark_tasks)
-            install_cluster >> benchmarks >> cleanup_cluster
+        installer = self._get_openshift_installer()
+        install_cluster = installer.get_install_task()
+        cleanup_cluster = installer.get_cleanup_task()
+        with TaskGroup("benchmarks", prefix_group_id=False, dag=self.dag) as benchmarks:
+            benchmark_tasks = self._get_ripsaw().get_benchmarks()
+            chain(*benchmark_tasks)
+        install_cluster >> benchmarks >> cleanup_cluster
 
     def _get_openshift_installer(self):
         return openshift.OpenshiftInstaller(self.dag, self.version, self.platform, self.profile)
