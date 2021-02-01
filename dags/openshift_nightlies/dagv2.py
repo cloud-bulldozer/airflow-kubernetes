8953c6635f477fbf7ccf4fed8ed1ef71fbba8212
Keith Whitley <kwhitley@redhat.com>
2021-02-01 10:34:30 -0500

    using TaskGroups for benchmarks

--- dags/openshift_nightlies/dagv2.py
+++ dags/openshift_nightlies/dagv2.py
@@ -52,7 +52,7 @@ class OpenshiftNightlyDAG():
         installer = self._get_openshift_installer()
         install_cluster = installer.get_install_task()
         cleanup_cluster = installer.get_cleanup_task()
-        with TaskGroup("benchmarks", prefix_group_id=False) as benchmarks:
+        with TaskGroup("benchmarks", prefix_group_id=False, dag=self.dag) as benchmarks:
             benchmark_tasks = self._get_ripsaw().get_benchmarks()
             chain(*benchmark_tasks)
         install_cluster >> benchmarks >> cleanup_cluster
