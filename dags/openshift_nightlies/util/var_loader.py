ecaca1edf05091390e15d97487259d50d44802c2
Keith Whitley <kwhitley@redhat.com>
2021-02-19 12:59:58 -0500

    adding retries to the release finder

--- dags/openshift_nightlies/util/var_loader.py
+++ dags/openshift_nightlies/util/var_loader.py
@@ -1,12 +1,29 @@
 import json
 from util import constants
 import requests
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 
 def get_latest_release_from_stream(base_url, release_stream):
+    session = requests.Session()
+    retries = Retry(
+        total=5,
+        backoff_factor=1,
+        status_forcelist=[ 404, 500, 502, 503, 504 ]
+    )
+
     url = f"{base_url}/{release_stream}/latest"
     payload = requests.get(url).json()
+
     latest_accepted_release = payload["name"]
     latest_accepted_release_url = payload["downloadURL"]
+
+    # hit the release url to ensure the release app has started serving the artifacts
+    session.mount('http://', HTTPAdapter(max_retries=retries))
+    session.get(latest_accepted_release_url)
+
+    # This should retry a total a 5 times until the request returns 200, indicating the binaries have been extracted. 
+    session.get(f"{latest_accepted_release_url}/openshift-client-linux-{latest_accepted_release}.tar.gz")
     return {
         "openshift_client_location": f"{latest_accepted_release_url}/openshift-client-linux-{latest_accepted_release}.tar.gz",
         "openshift_install_binary_url": f"{latest_accepted_release_url}/openshift-install-linux-{latest_accepted_release}.tar.gz"
