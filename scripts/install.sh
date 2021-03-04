b1665565d9c50ad2f222a0c09dfd293e5ecd275c
Keith Whitley <kwhitley@redhat.com>
2021-03-04 14:59:27 -0500

    pushing new install script and fixes to chart/deployments

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-set -x 
+ 
 usage() { printf "Usage: ./install.sh -u USER -b BRANCH \nUser/Branch should point to your fork of the airflow-kubernetes repository" 1>&2; exit 1; }
 GIT_ROOT=$(git rev-parse --show-toplevel)
 while getopts b:u:c: flag
@@ -64,8 +63,49 @@ install_perfscale(){
 
 }
 
+output_info() {
+    _argo_url=$(oc get route/argocd -o jsonpath='{.spec.host}' -n argocd)
+    _argo_user="admin"
+    _argo_password=$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o jsonpath='{.items[].metadata.name'})
+
+    printf "\n\n ArgoCD Configs"
+    printf "\n Host: $_argo_url \n User: $_argo_user \n Password: $_argo_password"
+
+    _airflow_url=$(oc get route/airflow -o jsonpath='{.spec.host}' -n airflow)
+    _airflow_user="admin"
+    _airflow_password="admin"
+
+    printf "\n\n Airflow Configs"
+    printf "\n Host: $_airflow_url \n User: $_airflow_user \n Password: $_airflow_password"
+
+
+    _logging_elastic_url=$(oc get route/logging-elastic -o jsonpath='{.spec.host}' -n logging)
+    _logging_kibana_url=$(oc get route/logging-kibana -o jsonpath='{.spec.host}' -n logging)
+    _logging_elastic_user="elastic"
+    _logging_elastic_password=$(kubectl get secret/logging-es-elastic-user -o jsonpath='{.data.elastic}' -n logging | base64 --decode)
+
+    printf "\n\n Logging Elasticsearch Configs"
+    printf "\n Elastic Host: $_logging_elastic_url \n Kibana Host: $_logging_kibana_url \n User: $_logging_elastic_user \n Password: $_logging_elastic_password"
+
+    _results_elastic_url=$(oc get route/perf-results-elastic -o jsonpath='{.spec.host}' -n perf-results)
+    _results_kibana_url=$(oc get route/perf-results-kibana -o jsonpath='{.spec.host}' -n perf-results)
+    _results_elastic_user="elastic"
+    _results_elastic_password=$(kubectl get secret/perf-results-es-elastic-user -o jsonpath='{.data.elastic}' -n perf-results | base64 --decode)
+
+    printf "\n\n Results Elasticsearch Configs"
+    printf "\n Elastic Host: $_results_elastic_url \n Kibana Host: $_results_kibana_url \n User: $_results_elastic_user \n Password: $_results_elastic_password"
+
+    _results_dashboard_url=$(oc get route/perf-dashboard -o jsonpath='{.spec.host}' -n perf-results)
+    _results_api_url=$(oc get route/perf-dashboard-api -o jsonpath='{.spec.host}' -n perf-results)
+
+    printf "\n\n Results Dashboard Configs"
+    printf "\n Dashboard URL: $_results_dashboard_url \n API Endpoint: $_results_api_url \n"
+
+}
+
 install_helm
 create_namespaces
 add_privileged_service_accounts
 install_argo
 install_perfscale
+output_info
