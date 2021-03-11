2fc22bd3121eb402476d0d888531e12d55a226e4
Keith Whitley <kwhitley@redhat.com>
2021-03-11 15:10:18 -0500

    adding waiter to the uninstall

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 helm delete perfscale -n argocd
+
+while [ $(kubectl get apps -A | wc -l) != "0" ];
+do 
+    sleep 5
+done
+
 kubectl delete namespace fluentd || true
 kubectl delete namespace airflow || true
 kubectl delete namespace logging || true
