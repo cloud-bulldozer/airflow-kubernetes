7a8239e7c8cca76422a7a6cd65aaeeb1eabad233
Keith Whitley <kwhitley@redhat.com>
2021-05-10 13:50:26 -0400

    letting kubeconfig be writeable pt 2

--- dags/openshift_nightlies/scripts/run_benchmark.sh
+++ dags/openshift_nightlies/scripts/run_benchmark.sh
@@ -15,7 +15,9 @@ setup(){
     mkdir /home/airflow/workspace
     cd /home/airflow/workspace
     git clone https://github.com/cloud-bulldozer/e2e-benchmarking
-    export KUBECONFIG=/home/airflow/.kube/config
+
+    cp /home/airflow/.kube/config /home/airflow/workspace/kubeconfig
+    export KUBECONFIG=/home/airflow/workspace/kubeconfig
     export BUILD_NUMBER=test
     export RUN_ID=${AIRFLOW_CTX_DAG_ID}/${AIRFLOW_CTX_DAG_RUN_ID}/$AIRFLOW_CTX_TASK_ID
 
