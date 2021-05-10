f15ab40a03edbc62a8ab151093dddebd78783d92
Keith Whitley <kwhitley@redhat.com>
2021-05-10 13:21:53 -0400

    letting kubeconfig be writeable

--- dags/openshift_nightlies/util/kubeconfig.py
+++ dags/openshift_nightlies/util/kubeconfig.py
@@ -26,7 +26,7 @@ def get_kubeconfig_volume_mount():
     return k8s.V1VolumeMount(
         name="kubeconfig",
         mount_path="/home/airflow/.kube",
-        read_only=True
+        read_only=False
     )
 
 def get_empty_dir_volume_mount():
