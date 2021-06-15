a1e8468a1a2eae370e7dd09e408a2da7126e0943
Keith Whitley <kwhitley@redhat.com>
2021-06-15 14:49:15 -0400

    hotfixing script

--- scripts/playground/cleanup.sh
+++ scripts/playground/cleanup.sh
@@ -5,8 +5,8 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 source $GIT_ROOT/scripts/common.sh
 _remote_origin_url=$(git config --get remote.origin.url)
 _remote_user=$(git config --get remote.origin.url | cut -d'/' -f4 | tr '[:upper:]' '[:lower:]')
-_branch=$(git branch --show-current)
-_airflow_namespace=$_remote_user-${_branch//_/-}-airflow
+_branch=$(git branch --show-current | tr '[:upper:]' '[:lower:]' | sed -r 's/[_]+/-/g')
+_airflow_namespace=$_remote_user-$_branch-airflow
 _cluster_domain=$(oc get ingresses.config.openshift.io/cluster -o jsonpath='{.spec.domain}')
 
 
