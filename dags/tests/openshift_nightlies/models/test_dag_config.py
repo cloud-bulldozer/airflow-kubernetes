36fd7a9bfc294e9b2e366a1a5aedac89c5409894
Keith Whitley <kwhitley@redhat.com>
2021-08-17 13:47:32 -0400

    adding initial tests

--- dags/tests/openshift_nightlies/models/test_dag_config.py
+++ dags/tests/openshift_nightlies/models/test_dag_config.py
@@ -3,5 +3,11 @@ from openshift_nightlies.models.dag_config import DagConfig
 class TestDagConfig():
     def test_create_empty_config(self):
         my_config = DagConfig()
-        assert True
+        assert my_config.default_args['owner'] == 'airflow'
 
+    def test_create_config_default_args(self):
+        my_custom_args = {
+            "foo": "bar"
+        }
+        my_config = DagConfig(default_args=my_custom_args)
+        assert my_config.default_args['foo'] == 'bar'
