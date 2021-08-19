7f26050ff0f694ee8cb2e91834bd3a92bddc1563
Keith Whitley <kwhitley@redhat.com>
2021-08-19 15:29:06 -0400

    testing removing logging changes in python

--- dags/openshift_nightlies/dag.py
+++ dags/openshift_nightlies/dag.py
@@ -24,11 +24,6 @@ from openshift_nightlies.tasks.index import status
 from openshift_nightlies.util import var_loader, manifest, constants
 from abc import ABC, abstractmethod
 
-# Set Task Logger to INFO for better task logs
-log = logging.getLogger("airflow.task.operators")
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
-log.addHandler(handler)
 
 # This Applies to all DAGs
 
