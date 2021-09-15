c2d765037e87f7df50e626e0568bf53a297afcef
Murali Krishnasamy <mukrishn@redhat.com>
2021-09-15 14:38:03 -0400

    cleaned up all test configs

--- dags/openshift_nightlies/scripts/run_benchmark.sh
+++ dags/openshift_nightlies/scripts/run_benchmark.sh
@@ -59,7 +59,7 @@ run_baremetal_benchmark(){
     rm -rf /home/kni/ci_${TASK_GROUP}_workspace
     mkdir /home/kni/ci_${TASK_GROUP}_workspace
     pushd /home/kni/ci_${TASK_GROUP}_workspace
-    git clone -b master https://github.com/jdowni000/e2e-benchmarking.git
+    git clone -b master https://github.com/cloud-bulldozer/e2e-benchmarking.git
 
     pushd e2e-benchmarking/workloads/$workload
     eval "$command"
