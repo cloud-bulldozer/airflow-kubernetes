55fe06ee33e638cf975c0dc6beb3794a98fb9fec
Keith Whitley <kwhitley@redhat.com>
2021-09-28 16:41:44 -0400

    testing xcom for uuid

--- dags/openshift_nightlies/tasks/index/status.py
+++ dags/openshift_nightlies/tasks/index/status.py
@@ -31,7 +31,7 @@ class StatusIndexer():
             "RELEASE_STREAM": self.release.release_stream,
             "TASK": self.task
         }
-        
+
         self.git_user = var_loader.get_git_user()
         if self.git_user == 'cloud-bulldozer':
             self.env["ES_INDEX"] = "perf_scale_ci"
@@ -50,7 +50,7 @@ class StatusIndexer():
         return BashOperator(
             task_id=f"index_{self.task}",
             depends_on_past=False,
-            bash_command=f' UUID={{ ti.xcom_pull({self.task}) }}{constants.root_dag_dir}/scripts/index.sh ',
+            bash_command=f'UUID={{{{ ti.xcom_pull({self.task}) }}}} {constants.root_dag_dir}/scripts/index.sh ',
             retries=3,
             dag=self.dag,
             trigger_rule="all_done",
