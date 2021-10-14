7c38de48597e82280bb2655c220869e295feb2c6
Keith Whitley <kwhitley@redhat.com>
2021-10-14 12:02:40 -0400

    adding node cpu

--- dags/reports/pipeline/metrics/control_plane.py
+++ dags/reports/pipeline/metrics/control_plane.py
@@ -9,7 +9,6 @@ def get_average_cpu_of_control_plane_apps(report, prom_client):
     start = util.parse_timestamp(report['metadata']['start_date'])
     end = util.parse_timestamp(report['metadata']['end_date'])
     cluster = report['cluster_name']
-    metric = "container_cpu_usage_seconds_total"
     labels = f'name!="", openshift_cluster_name="{cluster}", container!="POD",namespace=~"openshift-(etcd|oauth-apiserver|.*apiserver|ovn-kubernetes|sdn|ingress|authentication|.*controller-manager|.*scheduler|monitoring|image-registry|operator-lifecycle-manager)"'
     query = f'sum(irate(container_cpu_usage_seconds_total{{{labels}}}[2m])) by (namespace)'
     metric_data = prom_client.custom_query_range(query=query, start_time=start, end_time=end, step=5)
@@ -27,7 +26,6 @@ def get_average_memory_of_control_plane_apps(report, prom_client):
     start = util.parse_timestamp(report['metadata']['start_date'])
     end = util.parse_timestamp(report['metadata']['end_date'])
     cluster = report['cluster_name']
-    metric = "container_memory_working_set_bytes"
     labels = f'name!="", openshift_cluster_name="{cluster}", container!="POD",namespace=~"openshift-(etcd|oauth-apiserver|.*apiserver|ovn-kubernetes|sdn|ingress|authentication|.*controller-manager|.*scheduler|monitoring|image-registry|operator-lifecycle-manager)"'
     query = f'sum(container_memory_working_set_bytes{{{labels}}}) by (namespace)'
     metric_data = prom_client.custom_query_range(query=query, start_time=start, end_time=end, step=5)
@@ -38,3 +36,45 @@ def get_average_memory_of_control_plane_apps(report, prom_client):
         }
 
     return result_set
+
+
+
+def get_masters(report, prom_client):
+    start = util.parse_timestamp(report['metadata']['start_date'])
+    end = util.parse_timestamp(report['metadata']['end_date'])
+    cluster = report['cluster_name']
+    labels = f'openshift_cluster_name="{cluster}", role="master"'
+    query = f'sum(kube_node_role{{{labels}}}) by (node)'
+    metric_data = prom_client.custom_query_range(query=query, start_time=start, end_time=end, step=5)
+    return [series['metric']['node'] for series in metric_data]
+
+def get_average_cpu_of_master_nodes(report, prom_client):
+    start = util.parse_timestamp(report['metadata']['start_date'])
+    end = util.parse_timestamp(report['metadata']['end_date'])
+    cluster = report['cluster_name']
+    master_nodes = get_masters(report, prom_client)
+    labels = f'openshift_cluster_name="{cluster}", instance=~"{"|".join(master_nodes)}", mode!="idle"'
+    query = f'sum(irate(node_cpu_seconds_total{{{labels}}}[2m]))'
+    print(query)
+    metric_data = prom_client.custom_query_range(query=query, start_time=start, end_time=end, step=5)
+    return {
+        "average": util.aggregate_metrics("average", metric_data[0]['values']),
+        "max": util.aggregate_metrics("max", metric_data[0]['values'])
+    }
+
+# def get_average_memory_of_master_nodes(report, prom_client):
+#     result_set = {}
+
+#     start = util.parse_timestamp(report['metadata']['start_date'])
+#     end = util.parse_timestamp(report['metadata']['end_date'])
+#     cluster = report['cluster_name']
+#     labels = f'name!="", openshift_cluster_name="{cluster}", container!="POD",namespace=~"openshift-(etcd|oauth-apiserver|.*apiserver|ovn-kubernetes|sdn|ingress|authentication|.*controller-manager|.*scheduler|monitoring|image-registry|operator-lifecycle-manager)"'
+#     query = f'sum(container_memory_working_set_bytes{{{labels}}}) by (namespace)'
+#     metric_data = prom_client.custom_query_range(query=query, start_time=start, end_time=end, step=5)
+#     for series in metric_data:
+#         result_set[series['metric']['namespace']] = {
+#             "average": util.aggregate_metrics("average", series['values']),
+#             "max": util.aggregate_metrics("max", series['values'])
+#         }
+
+#     return result_set
