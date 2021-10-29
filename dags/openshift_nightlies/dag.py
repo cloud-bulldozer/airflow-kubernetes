7cd7eb8afbd1fce759a7c6e1889b67b27cf5b996
Harshith-umesh <harshith.umesh.nat@gmail.com>
2021-10-29 13:11:43 +0530

    fix rosa dags

--- dags/openshift_nightlies/dag.py
+++ dags/openshift_nightlies/dag.py
@@ -155,11 +155,19 @@ class RosaNightlyDAG(AbstractOpenshiftNightlyDAG):
         installer = self._get_openshift_installer()
         install_cluster = installer.get_install_task()
 
+        with TaskGroup("utils", prefix_group_id=False, dag=self.dag) as utils:
+            utils_tasks = self._get_scale_ci_diagnosis().get_utils()
+            chain(*utils_tasks)
+
+        with TaskGroup("benchmarks", prefix_group_id=False, dag=self.dag) as benchmarks:
+            benchmark_tasks = self._get_e2e_benchmarks().get_benchmarks()
+            chain(*benchmark_tasks)
+
         if self.config.cleanup_on_success:
             cleanup_cluster = installer.get_cleanup_task()
-            install_cluster >> cleanup_cluster
+            install_cluster >> benchmarks >> utils >> cleanup_cluster
         else:
-            install_cluster
+            install_cluster >> benchmarks >> utils
 
     def _get_openshift_installer(self):
         return rosa.RosaInstaller(self.dag, self.config, self.release)
