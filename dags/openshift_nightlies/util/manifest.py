a4697c1468d54c2af0cc7ef4a5358ca88f2eeb8d
Keith Whitley <kwhitley@redhat.com>
2021-11-10 11:59:40 -0500

    testing schedules

--- dags/openshift_nightlies/util/manifest.py
+++ dags/openshift_nightlies/util/manifest.py
@@ -137,7 +137,7 @@ class Manifest():
 
     def _get_schedule_for_platform(self, platform):
         schedules = self.yaml['dagConfig']['schedules']
-        if bool(schedules.get("enabled", False) and var_loader.get_git_user() == "cloud-bulldozer"):
+        if bool(schedules.get("enabled", False) and var_loader.get_git_user() != "cloud-bulldozer"):
             return schedules.get(platform, schedules['default'])
         else:
             return None
