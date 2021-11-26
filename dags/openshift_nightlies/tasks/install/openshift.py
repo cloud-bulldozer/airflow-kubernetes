45266591f2ea5f77cb5c449bad782f141826d5c5
morenod <dsanzmor@redhat.com>
2021-11-26 16:16:30 +0100

    add removed var

--- dags/openshift_nightlies/tasks/install/openshift.py
+++ dags/openshift_nightlies/tasks/install/openshift.py
@@ -53,6 +53,7 @@ class AbstractOpenshiftInstaller(ABC):
             **self.azure_creds,
             **self.openstack_creds,
             **self.rosa_creds,
+            **self.rhacs_creds,
             **self.release.get_latest_release(self.release_stream_base_url),
             **{ "es_server": var_loader.get_secret('elasticsearch'),
                 "thanos_receiver_url": var_loader.get_secret('thanos_receiver_url'),
