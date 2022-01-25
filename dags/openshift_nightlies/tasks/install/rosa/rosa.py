03726762d287f21fa4dfa85c1b2c356bb5d5f192
Sai Sindhur Malleni <smalleni@redhat.com>
2022-01-25 09:33:11 -0600

    Enable capability to consume ROSA masterbranch (#108)
    Right now the cli option in ROSA to deploy OVN clusters is not
    in a tagged build. In the future, this might be the case for other
    features. It would be nice to have the ability to consume master branch
    ROSA. This commit involves changes in the Dockerfile for the
    airflow-managed-services image because we would need go in the image and
    right now with multi-stage builds we are only using the golang image for
    building osde2e binary and then copying  it off to the airflow base
    image.
    
    This commit also removes all references to osde2d since its no longer
    required after ROSA supports OVNKubernetes installs.
    
    Signed-off-by: Sai Sindhur Malleni <smalleni@redhat.com>

--- dags/openshift_nightlies/tasks/install/rosa/rosa.py
+++ dags/openshift_nightlies/tasks/install/rosa/rosa.py
@@ -25,10 +25,7 @@ class RosaInstaller(AbstractOpenshiftInstaller):
     # Create Airflow Task for Install/Cleanup steps
     def _get_task(self, operation="install", trigger_rule="all_success"):
         self._setup_task(operation=operation)
-        if (self.vars['rosa_installation_method'] == "osde2e"):
-            command=f"{constants.root_dag_dir}/scripts/install/osde2e.sh -v {self.release.version} -j /tmp/{self.release_name}-{operation}-task.json -o {operation}"
-        else:
-            command=f"{constants.root_dag_dir}/scripts/install/rosa.sh -v {self.release.version} -j /tmp/{self.release_name}-{operation}-task.json -o {operation}"
+        command=f"{constants.root_dag_dir}/scripts/install/rosa.sh -v {self.release.version} -j /tmp/{self.release_name}-{operation}-task.json -o {operation}"
 
         return BashOperator(
             task_id=f"{operation}",
