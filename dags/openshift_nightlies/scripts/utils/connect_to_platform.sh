786486b37ca2069b16eb869f692382f54c5ca96a
Keith Whitley <kwhitley@redhat.com>
2022-01-27 15:02:34 -0500

    adding platform connector task

---
+++ dags/openshift_nightlies/scripts/utils/connect_to_platform.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+generate_external_labels(){
+    # Get OpenShift cluster details
+    CLUSTER_NAME=$(oc get infrastructure cluster -o jsonpath='{.status.infrastructureName}')
+    OPENSHIFT_VERSION=$(oc version -o json | jq -r '.openshiftVersion')
+    NETWORK_TYPE=$(oc get network.config/cluster -o jsonpath='{.status.networkType}')
+    PLATFORM=$(oc get infrastructure cluster -o jsonpath='{.status.platformStatus.type}')
+    DAG_ID=${AIRFLOW_CTX_DAG_ID}
+
+}
+
+
+install_grafana_agent(){
+    envsubst < templates/grafana-agent.yaml | kubectl apply -f -
+}
+
+install_promtail(){
+    # TODO 
+}
+
+
+setup(){
+    cp /home/airflow/auth/config /home/airflow/workspace/config
+    export KUBECONFIG=/home/airflow/workspace/config
+    curl -sS https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz | tar xz oc
+    export PATH=$PATH:$(pwd)
+
+    if [[ ! -z "$KUBEADMIN_PASSWORD" ]]; then
+        oc login -u kubeadmin -p $KUBEADMIN_PASSWORD --insecure-skip-tls-verify
+    fi
+}
+
+
+setup
+generate_external_labels
+install_grafana_agent
+
+
---
+++ dags/openshift_nightlies/tasks/utils/platform_connector.py
@@ -0,0 +1,37 @@
+from os import environ
+from openshift_nightlies.util import var_loader, executor, constants
+from openshift_nightlies.models.release import OpenshiftRelease
+from openshift_nightlies.models.dag_config import DagConfig
+
+
+from airflow.operators.bash import BashOperator
+
+
+# Connect Installed Cluster to PerfScale Platform to send metrics and logs. 
+
+class PlatformConnectorTask():
+    def __init__(self, dag, config: DagConfig, release: OpenshiftRelease):
+        
+        # General DAG Configuration
+        self.dag = dag
+        self.release = release
+        self.config = config
+        self.exec_config = executor.get_executor_config_with_cluster_access(self.config, self.release)
+
+        # Specific Task Configuration
+        self.env = {
+            "THANOS_RECEIVER_URL": var_loader.get_secret("thanos_receiver_url")
+        }
+
+    def get_task(self):
+        env = {**self.env, **{"KUBEADMIN_PASSWORD": environ.get("KUBEADMIN_PASSWORD", "")}}
+        return BashOperator(
+            task_id="connect-to-platform",
+            depends_on_past=False,
+            bash_command=f"{constants.root_dag_dir}/scripts/utils/connect_to_platform.sh ",
+            retries=3,
+            dag=self.dag,
+            env=env,
+            cwd=f"{constants.root_dag_dir}/scripts/utils ",
+            executor_config=self.exec_config
+        )
