b6f7fbb5c5deb14ca0a4a9a76342985751b41aef
Harshith-umesh <harshith.umesh.nat@gmail.com>
2022-02-02 14:53:22 +0530

    download oc client

--- dags/openshift_nightlies/scripts/install/prebuilt.sh
+++ dags/openshift_nightlies/scripts/install/prebuilt.sh
@@ -16,6 +16,10 @@ create_login_secrets(){
     echo $PWD 
     rm -f ~/.kube/config 
 
+    curl -sS https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz | tar xz oc
+
+    export PATH=$PATH:$(pwd)
+
     oc login -u ${user} -p ${pass} ${url} --insecure-skip-tls-verify
     ls ~/
 
