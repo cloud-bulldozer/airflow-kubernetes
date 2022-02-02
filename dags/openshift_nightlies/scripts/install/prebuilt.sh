661da270d45226754c074787b8cdf2e8a97c0e94
Harshith-umesh <harshith.umesh.nat@gmail.com>
2022-02-02 12:28:24 +0530

    add oc login

--- dags/openshift_nightlies/scripts/install/prebuilt.sh
+++ dags/openshift_nightlies/scripts/install/prebuilt.sh
@@ -4,5 +4,9 @@ set -ex
 create_login_secrets(){
     echo {{ params.KUBEUSER }}; echo {{ params.KUBEPASSWORD }}; echo {{ params.KUBEURL }};
     ls ~/.kube/
+    rm -f ~/.kube/config 
+
+    oc login -u {{ params.KUBEUSER }} -p {{ params.KUBEPASSWORD }} {{ params.KUBEURL }}
+    ls ~/.kube/
 }
 
