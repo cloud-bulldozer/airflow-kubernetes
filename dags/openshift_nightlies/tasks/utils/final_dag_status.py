028cc4c72cac8081c67df40a08ec2cd028a3cd8a
Harshith-umesh <harshith.umesh.nat@gmail.com>
2022-02-10 15:16:24 +0530

    mark dag failed if any upstream task fails

---
+++ dags/openshift_nightlies/tasks/utils/final_dag_status.py
@@ -0,0 +1,24 @@
+from os import environ
+from openshift_nightlies.util import var_loader, executor, constants
+from openshift_nightlies.models.release import OpenshiftRelease
+from openshift_nightlies.models.dag_config import DagConfig
+
+
+from airflow.operators.python import PythonOperator
+
+
+def final_status(**kwargs):
+    for task_instance in kwargs['dag_run'].get_task_instances():
+        if task_instance.current_state() != State.SUCCESS and task_instance.task_id != kwargs['task_instance'].task_id:
+            raise Exception("Task {} failed. Failing this DAG run".format(task_instance.task_id))
+
+
+
+def get_task(dag):
+    return PythonOperator(
+        task_id='final_status',
+        provide_context=True,
+        python_callable=final_status,
+        trigger_rule='all_done',
+        dag=dag,
+    )
