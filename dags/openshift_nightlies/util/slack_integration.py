49fba01f66214a0e399ecb31a324541b5fcc627c
Harshith-umesh <harshith.umesh.nat@gmail.com>
2022-02-11 13:36:10 +0530

    add shortened hyperlink

--- dags/openshift_nightlies/util/slack_integration.py
+++ dags/openshift_nightlies/util/slack_integration.py
@@ -2,34 +2,30 @@ from airflow.hooks.base import BaseHook
 from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
 import logging
 from os import environ
+from openshift_nightlies.util import var_loader
 
 SLACK_CONN_ID = 'slack'
-
-# Used to get the git user for the repo the dags live in. 
-def get_git_user():
-    git_repo = environ['GIT_REPO']
-    git_path = git_repo.split("https://github.com/")[1]
-    git_user = git_path.split('/')[0]
-    return git_user.lower()
-
 
 def alert_members(context):
     if "rosa" in context.get('task_instance').dag_id or "rogcp" in context.get('task_instance').dag_id or "-aro-" in context.get('task_instance').dag_id:
         members=" @perfscale-managed-services-team" 
-    elif "aws" in context.get('task_instance').dag_id or "azure" in context.get('task_instance').dag_id or "-gcp-" in context.get('task_instance').dag_id or "baremetal" in context.get('task_instance').dag_id:
+    elif "awsf" in context.get('task_instance').dag_id or "azure" in context.get('task_instance').dag_id or "-gcp-" in context.get('task_instance').dag_id or "baremetal" in context.get('task_instance').dag_id:
         members=" @perfscale-core-team"
     elif "openstack" in context.get('task_instance').dag_id:
         members=" @asagtani @masco"
     else:
         members=""
-    return members            
+    return members    
+
+def get_hyperlink(context):
+    return '{"blocks":[{"type": "section","text":{"type": "mrkdwn","text": "<{0}|Dag Link>"}}]}'.format(context.get('task_instance').log_url)        
 
 
 def task_fail_slack_alert(context):
     slack_webhook_token = BaseHook.get_connection(SLACK_CONN_ID).password
-    if get_git_user() != "cloud-bulldozer":
-        print("Task Failed")
-        return
+    #if var_loader.get_git_user() != "cloud-bulldozer":
+    #    print("Task Failed")
+    #    return
     if "index" in context.get('task_instance').task_id:
         print("Index Task Failed")
         return
@@ -46,7 +42,7 @@ def task_fail_slack_alert(context):
             mem=alert_members(context),
             ti=context.get('task_instance'),
             exec_date=context.get('execution_date'),
-            log_url=context.get('task_instance').log_url,
+            log_url=get_hyperlink(context),
         )
     failed_alert = SlackWebhookOperator(
         task_id='slack_test',
