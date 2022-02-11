7c4241bfa7bae2368572262453d8c2e75b95a88b
Harshith-umesh <harshith.umesh.nat@gmail.com>
2022-02-11 12:01:52 +0530

    log all failed tasks rather than the first one

--- dags/openshift_nightlies/tasks/utils/final_dag_status.py
+++ dags/openshift_nightlies/tasks/utils/final_dag_status.py
@@ -8,9 +8,15 @@ from airflow.operators.python import PythonOperator
 
 
 def final_status(**kwargs):
+    failed_tasks=[]
     for task_instance in kwargs['dag_run'].get_task_instances():
-        if task_instance.current_state() != 'success' and task_instance.task_id != kwargs['task_instance'].task_id:
-            raise Exception("Task {} failed. Failing this DAG run".format(task_instance.task_id))
+        if "index" in task_instance.task_id:
+            continue
+        elif task_instance.current_state() != 'success' and task_instance.task_id != kwargs['task_instance'].task_id:
+            failed_tasks.append(task_instance.task_id)
+
+    if len(failed_tasks) > 0:
+        raise Exception("Tasks {} failed. Failing this DAG run".format(failed_tasks))
 
 
 
