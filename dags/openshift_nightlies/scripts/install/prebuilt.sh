438abb02465c4a47c8869fb3e2500fdecbe29d9c
Harshith-umesh <harshith.umesh.nat@gmail.com>
2022-02-16 21:26:56 +0530

    add force cleanup logic to platform-connector task

--- dags/openshift_nightlies/scripts/install/prebuilt.sh
+++ dags/openshift_nightlies/scripts/install/prebuilt.sh
@@ -11,23 +11,18 @@ do
 done
 
 create_login_secrets(){
-    echo ${user}; echo ${pass}; echo ${url};   
-    echo $PWD 
-    
+    echo ${user}; echo ${pass}; echo ${url};     
     curl -sS https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz | tar xz oc
-
     export PATH=$PATH:$(pwd)
     
     mkdir workspace
     cd workspace 
     export KUBECONFIG=kubeconfig
     oc login -u ${user} -p ${pass} ${url} --insecure-skip-tls-verify
-    ls 
-
+    
     unset KUBECONFIG
     kubectl create secret generic ${KUBEADMIN_NAME} --from-literal=KUBEADMIN_PASSWORD=$pass
     kubectl create secret generic ${KUBECONFIG_NAME} --from-file=config=kubeconfig
-    
     cd .. 
 }
 
