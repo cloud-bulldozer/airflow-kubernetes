95885f1f37041af3876adfab2ad73e3b3c5aaf41
Raul Sevilla <rsevilla@redhat.com>
2022-05-10 14:19:55 +0200

    Remove test_get_overridden_secret test
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- dags/tests/openshift_nightlies/util/test_var_loader.py
+++ dags/tests/openshift_nightlies/util/test_var_loader.py
@@ -7,20 +7,11 @@ class TestVarLoader():
         environ['GIT_REPO'] = "https://github.com/FOO/repo"
         assert var_loader.get_git_user() == "foo"
 
-    def test_get_overrides_empty(self, mocker):
-        mocker.patch('airflow.models.Variable.get', side_effect=KeyError())
-        assert var_loader.get_overrides() == {}
-
 
     def test_get_secret(self, mocker):
-        mocker.patch('openshift_nightlies.util.var_loader.get_overrides', return_value={})
         mocker.patch('airflow.models.Variable.get')
         var_loader.get_secret("foo")
         Variable.get.assert_called_once_with("foo", deserialize_json=False)
-
-    def test_get_overridden_secret(self, mocker):
-        mocker.patch('openshift_nightlies.util.var_loader.get_overrides', return_value={"foo": "bar"})
-        assert var_loader.get_secret("foo") == "bar"
 
     def test_get_default_task_vars(self, valid_openshift_release, test_tasks_dir):
         assert var_loader.get_default_task_vars(valid_openshift_release, task="test", task_dir=str(test_tasks_dir)) == {
