4c6b5d4c1d36cb2159ecadd24cb54d6c18f4811c
dry923 <rzaleski@redhat.com>
2022-07-11 10:38:24 -0400

    Update hypershift install to use specific operator image

--- dags/openshift_nightlies/scripts/install/hypershift.sh
+++ dags/openshift_nightlies/scripts/install/hypershift.sh
@@ -103,11 +103,12 @@ setup(){
 }
 
 install(){
+    export HYPERSHIFT_OPERATOR_VERSION=$(cat ${json_file} | jq -r .hypershift_operator_version)
     echo "Create S3 bucket.."
     aws s3api create-bucket --acl public-read --bucket $MGMT_CLUSTER_NAME-aws-rhperfscale-org --create-bucket-configuration LocationConstraint=$AWS_REGION --region $AWS_REGION || true
     echo "Wait till S3 bucket is ready.."
     aws s3api wait bucket-exists --bucket $MGMT_CLUSTER_NAME-aws-rhperfscale-org 
-    hypershift install --oidc-storage-provider-s3-bucket-name $MGMT_CLUSTER_NAME-aws-rhperfscale-org --oidc-storage-provider-s3-credentials aws_credentials --oidc-storage-provider-s3-region $AWS_REGION  --enable-ocp-cluster-monitoring --metrics-set=All
+    hypershift install --hypershift-image $HYPERSHIFT_OPERATOR_VERSION  --oidc-storage-provider-s3-bucket-name $MGMT_CLUSTER_NAME-aws-rhperfscale-org --oidc-storage-provider-s3-credentials aws_credentials --oidc-storage-provider-s3-region $AWS_REGION  --enable-ocp-cluster-monitoring --metrics-set=All
     echo "Wait till Operator is ready.."
     cm=""
     while [[ $cm != "oidc-storage-provider-s3-config" ]]
