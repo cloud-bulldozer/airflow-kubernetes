a9f045d66694e65ad6d1fc26a2e6d63585f47b17
Murali Krishnasamy <mukrishn@redhat.com>
2022-07-12 19:03:27 -0400

    parallel cleanups fo hostedcluster

--- dags/openshift_nightlies/dag.py
+++ dags/openshift_nightlies/dag.py
@@ -218,15 +218,14 @@ class HypershiftNightlyDAG(AbstractOpenshiftNightlyDAG):
         rosa_post_installation = self._get_rosa_postinstall_setup()._get_rosa_postinstallation()
 
         if self.config.cleanup_on_success:
-            install_hosted_cluster = hosted_installer.get_hosted_install_task()
             cleanup_mgmt_cluster = mgmt_installer.get_cleanup_task()
-            cleanup_hosted_cluster = hosted_installer.get_cleanup_task()
-            for c_id, install_hc in install_hosted_cluster:
+            cleanup_hosted_cluster = hosted_installer.get_hosted_cleanup_task()
+            for c_id, install_hc, cleanup_hc in cleanup_hosted_cluster:
                 benchmark = self._add_benchmarks(task_group=c_id)
                 hc_connect_to_platform = self._get_hc_platform_connector(task_group=c_id).get_task()            
                 install_mgmt_cluster >> rosa_post_installation >> connect_to_platform
                 connect_to_platform >> install_hc >> [hc_connect_to_platform, benchmark]
-                [hc_connect_to_platform, benchmark] >> cleanup_hosted_cluster >> cleanup_mgmt_cluster
+                [hc_connect_to_platform, benchmark] >> cleanup_hc >> cleanup_mgmt_cluster
         else:
             install_hosted_cluster = hosted_installer.get_hosted_install_task()
             for c_id, install_hc in install_hosted_cluster:
