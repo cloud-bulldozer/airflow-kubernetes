89ecb23a67df6713b8266dea0f8efbee78ebc485
morenod <dsanzmor@redhat.com>
2022-09-14 11:45:13 +0200

    Lint hypershift.py file

--- dags/openshift_nightlies/tasks/install/hypershift/hypershift.py
+++ dags/openshift_nightlies/tasks/install/hypershift/hypershift.py
@@ -1,30 +1,19 @@
-import sys
-from os.path import abspath, dirname
-from os import environ
-
-from openshift_nightlies.util import var_loader, kubeconfig, constants, executor
+from openshift_nightlies.util import var_loader, constants, executor
 from openshift_nightlies.tasks.install.openshift import AbstractOpenshiftInstaller
 from common.models.dag_config import DagConfig
 from openshift_nightlies.models.release import OpenshiftRelease
-from openshift_nightlies.tasks.benchmarks import e2e
-
-import requests
 
 from airflow.operators.bash import BashOperator
-from airflow.models import Variable
-from airflow.models.baseoperator import chain
-from airflow.utils.task_group import TaskGroup
-from kubernetes.client import models as k8s
-
-import json
 
 # Defines Tasks for installation of Openshift Clusters
+
 
 class HypershiftInstaller(AbstractOpenshiftInstaller):
     def __init__(self, dag, config: DagConfig, release: OpenshiftRelease):
         super().__init__(dag, config, release)
         self.exec_config = executor.get_executor_config_with_cluster_access(self.dag_config, self.release, executor_image="airflow-managed-services")
-        self.hypershift_pull_secret = var_loader.get_secret("hypershift_pull_secret") 
+        self.hypershift_pull_secret = var_loader.get_secret(
+            "hypershift_pull_secret")
 
     def get_hosted_install_task(self):
         config = {
@@ -32,7 +21,8 @@ class HypershiftInstaller(AbstractOpenshiftInstaller):
         }
 
         for iteration in range(config['number_of_hosted_cluster']):
-            c_id = f"{'hosted-'+str(iteration+1)}" # adding 1 to name the cluster hosted-1, hosted-2..
+            # adding 1 to name the cluster hosted-1, hosted-2..
+            c_id = f"{'hosted-'+str(iteration+1)}"
             yield c_id, self._get_task(operation="install", id=c_id)
 
     def get_hosted_cleanup_task(self):
@@ -41,7 +31,8 @@ class HypershiftInstaller(AbstractOpenshiftInstaller):
         }
 
         for iteration in range(config['number_of_hosted_cluster']):
-            c_id = f"{'hosted-'+str(iteration+1)}" # adding 1 to name the cluster hosted-1, hosted-2..
+            # adding 1 to name the cluster hosted-1, hosted-2..
+            c_id = f"{'hosted-'+str(iteration+1)}"
             yield c_id, self._get_task(operation="install", id=c_id), self._get_task(operation="cleanup", id=c_id)
 
     def get_operator_cleanup_task(self):
@@ -62,12 +53,12 @@ class HypershiftInstaller(AbstractOpenshiftInstaller):
             "ES_SERVER": var_loader.get_secret('elasticsearch'),
             "ES_INDEX": "ripsaw-kube-burner",
             "THANOS_RECEIVER_URL": var_loader.get_secret("thanos_receiver_url"),
-            "PROM_URL": var_loader.get_secret("thanos_querier_url"),            
+            "PROM_URL": var_loader.get_secret("thanos_querier_url"),
             **self.env
         }
         self.env.update(self.dag_config.dependencies)
         env = {**self.env, **{"HOSTED_NAME": id}}
-        command=f"{constants.root_dag_dir}/scripts/install/hypershift.sh -v {self.release.version} -j /tmp/{self.release_name}-{operation}-task.json -o {operation}"
+        command = f"{constants.root_dag_dir}/scripts/install/hypershift.sh -v {self.release.version} -j /tmp/{self.release_name}-{operation}-task.json -o {operation}"
 
         task = BashOperator(
             task_id=f"{id}-{operation}",
