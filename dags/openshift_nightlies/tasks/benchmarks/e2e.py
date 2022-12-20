387b4f007ed11c8ad449dfdf80a357d2ae456577
Murali Krishnasamy <70236227+mukrishn@users.noreply.github.com>
2022-12-20 14:14:57 -0500

    updated prefix (#285)
    tested

--- dags/openshift_nightlies/tasks/benchmarks/e2e.py
+++ dags/openshift_nightlies/tasks/benchmarks/e2e.py
@@ -83,7 +83,7 @@ class E2EBenchmarks():
                 "THANOS_RECEIVER_URL": var_loader.get_secret("thanos_receiver_url"),
                 "PROM_URL": var_loader.get_secret("thanos_querier_url"),
                 "MGMT_CLUSTER_NAME": f"{mgmt_cluster_name}.*",
-                "HOSTED_CLUSTER_NS": f"clusters-hyp-{mgmt_cluster_name}-{self.task_group}",
+                "HOSTED_CLUSTER_NS": f"clusters-hcp-{mgmt_cluster_name}-{self.task_group}",
                 "MGMT_KUBECONFIG_SECRET": f"{release.get_release_name()}-kubeconfig",
                 **self._insert_kube_env()
             }
