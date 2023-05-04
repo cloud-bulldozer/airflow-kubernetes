99561eb7379e4cf1bb82eedaa11f0e6f980f02c7
Raul Sevilla <rsevilla@redhat.com>
2023-05-04 14:08:29 +0200

    Trigger must-gather as soon as at least one parent has failed
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- dags/openshift_nightlies/tasks/utils/scale_ci_diagnosis.py
+++ dags/openshift_nightlies/tasks/utils/scale_ci_diagnosis.py
@@ -3,20 +3,12 @@ from openshift_nightlies.util import var_loader, executor, constants
 from openshift_nightlies.models.release import OpenshiftRelease
 from common.models.dag_config import DagConfig
 
-import json
-from datetime import timedelta
 from airflow.operators.bash import BashOperator
-from airflow.models import Variable
-from airflow.models import DAG
-from airflow.utils.task_group import TaskGroup
-from kubernetes.client import models as k8s
-
-
 
 
 class Diagnosis():
-    def __init__(self, dag, config: DagConfig, release: OpenshiftRelease):
 
+    def __init__(self, dag, config: DagConfig, release: OpenshiftRelease):
         # General DAG Configuration
         self.dag = dag
         self.release = release
@@ -27,7 +19,7 @@ class Diagnosis():
         # Specific Task Configuration
         self.vars = var_loader.build_task_vars(
             release=self.release, task="utils")
-        self.git_name=self._git_name()
+        self.git_name = self._git_name()
         self.env = {
             "SNAPPY_DATA_SERVER_URL": self.snappy_creds['server'],
             "SNAPPY_DATA_SERVER_USERNAME": self.snappy_creds['username'],
@@ -37,22 +29,21 @@ class Diagnosis():
         }
         self.env.update(self.config.dependencies)
 
-
     def _git_name(self):
         git_username = var_loader.get_git_user()
         if git_username == 'cloud-bulldozer':
-            return f"perf-ci"
-        else: 
+            return "perf-ci"
+        else:
             return f"{git_username}"
 
     def get_utils(self):
         utils = self._get_utils(self.vars["utils"])
         return utils
 
-    def _get_utils(self,utils):
+    def _get_utils(self, utils):
         for index, util in enumerate(utils):
             utils[index] = self._get_util(util)
-        return utils 
+        return utils
 
     def _get_util(self, util):
         env = {**self.env, **util.get('env', {}), **{"ES_SERVER": var_loader.get_secret('elasticsearch')}, **{"KUBEADMIN_PASSWORD": environ.get("KUBEADMIN_PASSWORD", "")}}
@@ -63,7 +54,6 @@ class Diagnosis():
             retries=3,
             dag=self.dag,
             env=env,
-            executor_config=self.exec_config
+            executor_config=self.exec_config,
+            trigger_rule="one_failed"
         )
-
-
